29a7bc33e161d180726aa27c7b80fa230c01f04a
Ivan <ivaanpavlovic@gmail.com>
2025-07-29 00:09:00 +0100

    Add MIssion control for organization

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -16,42 +16,55 @@ class MissionsController < ApplicationController
       if @mission.save
         format.turbo_stream
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @mission.errors, status: :unprocessable_entity }
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace("mission-form", partial: "form"),
-                 status: :unprocessable_entity
-        end
+        render turbo_stream: turbo_stream.prepend(
+        "flash-messages",
+        partial: "shared/alert",
+        locals: { message: "Mission creation failed" }
+      )
       end
     end
   end
 
 def edit
   @mission = Mission.find(params[:id])
-
-  respond_to do |format|
-    format.turbo_stream # renders edit.turbo_stream.erb by default
-    format.html { render partial: "missions/form_modal", locals: { mission: @mission } }
-  end
 end
 
 
 
 def update
   @mission = Mission.find(params[:id])
+
   if @mission.update(mission_params)
-    redirect_to missions_path, notice: "Mission updated successfully."
+    respond_to do |format|
+      format.turbo_stream
+    end
   else
-    render partial: "missions/form_modal", locals: { mission: @mission }
+    render turbo_stream: turbo_stream.prepend(
+        "flash-messages",
+        partial: "shared/alert",
+        locals: { message: "Mission edit failed" }
+      )
   end
 end
 
   def destroy
+    @mission = Mission.find(params[:id])
+
+    respond_to do |format|
+      if @mission.destroy
+        format.turbo_stream
+      else
+      render turbo_stream: turbo_stream.prepend(
+        "flash-messages",
+        partial: "shared/alert",
+        locals: { message: "Mission deactivation failed" }
+      )
+      end
+    end
   end
 end
 
 private
 def mission_params
   params.require(:mission).permit(:title, :body, :owner_id)
-  # params.merge(owner_id: Current.user.id) if Current.user
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Start StimulusJS
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start();
+
+// Import and register all TailwindCSS Components or just the ones you need
+import { Alert} from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
