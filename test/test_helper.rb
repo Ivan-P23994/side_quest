8869e3ba0ec9c963fca5f3440bb2614a475015bb
Ivan <ivaanpavlovic@gmail.com>
2025-08-07 12:52:49 +0100

    fix upstream contract tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
 ENV["RAILS_ENV"] ||= "test"
+ENV["SEPOLIA_RPC_URL"] ||= "http://localhost:8545"
+ENV["VOUCHER_CONTRACT_ADDRESS"] ||= "0x0000000000000000000000000000000000000000"
+ENV["PRIVATE_KEY"] ||= "0x" + "1" * 64
+ENV["SIDE_QUEST_VOUCHER_ABI"] ||= {"dummy": "abi"}.to_json
+
 require_relative "../config/environment"
 require "rails/test_help"
 require 'mocha/minitest'
